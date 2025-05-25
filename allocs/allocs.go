f884b0af16a027e77883bcc85349424a81a0adb9
Vladislav <vladcherepanov0@gmail.com>
2025-05-25 22:54:31 +0300

    sum, allocs, fetchall tasks was solved

--- allocs/allocs.go
+++ allocs/allocs.go
@@ -1,9 +1,52 @@
 //go:build !solution
 
+// DONE *
 package allocs
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"sort"
+)
 
 // implement your Counter below
 
+type EnhacedCounter struct {
+	counts map[string]int
+}
+
 func NewEnhancedCounter() Counter {
-	return NewBaselineCounter()
+	return EnhacedCounter{
+		counts: map[string]int{},
+	}
+}
+
+func (ec EnhacedCounter) Count(r io.Reader) error {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		ec.counts[word]++
+	}
+
+	return scanner.Err()
+}
+
+func (ec EnhacedCounter) String() string {
+	// Предварительно выделяем память для всех ключей
+	keys := make([]string, 0, len(ec.counts))
+	for word := range ec.counts {
+		keys = append(keys, word)
+	}
+	sort.Strings(keys)
+
+	var buf bytes.Buffer
+
+	for _, key := range keys {
+		fmt.Fprintf(&buf, "word '%s' has %d occurrences\n", key, ec.counts[key])
+	}
+	return buf.String()
 }
--- fetchall/main.go
+++ fetchall/main.go
@@ -1,7 +1,57 @@
 //go:build !solution
 
+// DONE
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+type FetchProducer struct {
+	client http.Client
+	wg     sync.WaitGroup
+}
+
+type Fetcher interface {
+	fetch(url string) error
+}
+
+func NewFetchProducer() *FetchProducer {
+	return &FetchProducer{
+		client: http.Client{},
+	}
+}
+
+func main() {
+	producer := NewFetchProducer()
+	args := os.Args[1:]
+
+	for i := 0; i < len(args); i++ {
+		producer.wg.Add(1)
+		go producer.fetch(args[i])
+	}
+
+	producer.wg.Wait()
+
+}
+
+func (fp *FetchProducer) fetch(url string) {
+	defer fp.wg.Done()
+	startTime := time.Now()
+	r, err := fp.client.Get(url)
+
+	if err != nil {
+		fmt.Printf("Error while fetching url %s, error: %v\n", url, err)
+
+		return
+	}
+	defer r.Body.Close()
+	cl := r.Header.Get("content-length")
+
+	fmt.Printf("%v     %s     %s\n", time.Since(startTime), cl, url)
 }
